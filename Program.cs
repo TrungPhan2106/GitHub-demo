cc76f049f3cd5415e54f4318aa73e1ca1d07d1d2
TrungPhan2106 <141632871+TrungPhan2106@users.noreply.github.com>
2023-08-08 15:41:34 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@ using System;
 class Car
 {
     string color = "red";
+    string name = "BMW";
 
     static void Main(string[] args)
     {
         Car myObj = new Car();
         Console.WriteLine(myObj.color);
+        Console.WriteLine(myObj.name);
     }
 }
